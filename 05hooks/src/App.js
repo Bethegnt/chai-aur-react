8cd20c251e0c714c3e504244b5e12367648c3043
Bethegnt <tyagigagan30@gmail.com>
2023-10-26 10:33:42 +0530

    adding hooks for tests

--- 05hooks/src/App.js
+++ 05hooks/src/App.js
@@ -26,7 +26,9 @@ function App() {
   return (
     <>
     <h1 className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
-      
+      <div className='className="flex shadow rounded-lg overflow-hidden mb-4"'>
+        
+      </div>
       password generator
     </h1>
     </>
