00952a12504b198509a7cee06110c96b110d2da8
Bethegnt <tyagigagan30@gmail.com>
2023-12-06 13:37:09 +0530

    routing

--- 10reactrouter/src/Layout.jsx
+++ 10reactrouter/src/Layout.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Header from './components/Header/Header';
-import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
-const Layout = () => {
+function Layout() {
   return (
     <>
     <Header/>
-    <Outlet/>
-    <Footer/>
+    <Outlet />
+    <Footer />
     </>
   )
 }
 
-export default Layout;
+export default Layout
